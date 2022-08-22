77e521ec468ec8f0bfe4d61f89439881b42e2547
dimonEmiroff <mr-tvister@mail.ru>
2022-08-22 09:08:23 +0200

    addMobileMenu

--- menu.js
+++ menu.js
@@ -1,11 +1,30 @@
-const burger = document.querySelector(selector, '.head__burger')
-const menu = document.querySelector(selector, '.head__menu');
-burger.addEventLister('click', () => {
-  if (menu.ClassList[1] !== 'active') {
-    burger.classList.add('active')
-    menu.classList.add('active')
-  } else {
-    menu.classList.remove('active');
-  }
+const burger = document.querySelector('#head__burger')
+const menu = document.querySelector( '.head__menu');
+const body = document.querySelector('body')
 
-})
+burger.addEventListener("click", nav)
+function nav () {
+    // Переключаем стили элементов при клике
+    burger.classList.toggle("_active");
+    menu.classList.toggle("_active");
+    body.classList.toggle("noscroll");
+    renderMenu();
+}
+
+// Здесь мы рендерим элементы
+function renderMenu() {
+    menu.append(body);
+}
+const links = Array.from(menu.children);
+
+// Для каждого элемента меню при клике вызываем ф-ию
+links.forEach((link) => {
+    link.addEventListener("click", closeOnClick);
+});
+
+// Закрытие попапа при клике на меню
+function closeOnClick() {
+    burger.classList.remove("_active");
+    menu.classList.remove("_active");
+    body.classList.remove("noscroll");
+}
